397fc4099dc0cc89c658f616e5754ec2847ca72c
mushayed125 <kmr18adi@gmail.com>
2026-02-28 14:08:45 +0600

    ready for deployment

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@
 
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Fetch user/session
